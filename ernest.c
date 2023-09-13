caeab6a9e8401fb708c1504af04c2b13778c50c0
wandrest94 <ngangaernest9@gmail.com>
2023-09-13 20:07:56 +0300

    personal work

---
+++ ernest.c
@@ -0,0 +1,7 @@
+// this is my first personal c task
+// lookin forward to learning more
+#include <stdio.h>
+int main ()
+{
+	printf("hi,my name is Ernest\nI am 29 years old\nI am a student at ALX");
+}
